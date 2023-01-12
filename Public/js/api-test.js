033a88d45ef87244293a9fe5e3480821b8ab0b8b
Bubbl3Waffl3 <salahlfd0@gmail.com>
2023-01-12 15:21:41 +0100

    rick express

---
+++ Public/js/api-test.js
@@ -0,0 +1,37 @@
+
+
+
+let baseUrl = "https://rickandmortyapi.com/api";
+
+async function apiGET(route) {
+    try {
+        const response = await fetch(baseUrl + route)
+        const data = await response.json()
+        let characters = data.results
+        return characters
+        console.log(data)
+    } catch (error) { }
+}
+
+
+async function useAPI(params) {
+
+    let characters = await apiGET(params)
+    characters.forEach((element, index) => {
+
+        document.querySelector(".main-novedades").innerHTML += `<div>
+        <a href="./second_page.html?id=1"><img class="main-novedades-img"  src="${characters[index].image}"
+            alt="${characters[index].name}"></a>
+        <h3>"${characters[index].name}"</h3>
+    </div>`
+    });
+}
+
+
+function name(params) {
+    
+}
+
+useAPI("/character")
+
+
--- Public/js/index.js
+++ Public/js/index.js
@@ -1,30 +1,63 @@
 function printQuantity() {
   let cart = JSON.parse(localStorage.getItem("cart"))
-    if (cart == null) {
-        cart= []
-    }
-    
-    document.querySelector(".header-quantity").innerHTML =  cart.length
-    
+  if (cart == null) {
+    cart = []
+  }
+
+  document.querySelector(".header-quantity").innerHTML = cart.length
+
 }
 
 printQuantity()
 
-function sumaTotal () {
-    let suma = 0
-    let cart = JSON.parse(localStorage.getItem("cart"))
-    cart.forEach(element => { 
-      suma += element.price
-     
-     
-    }) 
-    suma = suma.toFixed(2)
-    document.querySelector(".total").innerHTML = suma + "€"
-    return suma 
-    
-}  
-  sumaTotal() 
-    
+function sumaTotal() {
+  let suma = 0
+  let cart = JSON.parse(localStorage.getItem("cart"))
+  cart.forEach(element => {
+    suma += element.price
+
+
+  })
+  suma = suma.toFixed(2)
+  document.querySelector(".total").innerHTML = suma + "€"
+  return suma
+
+}
+sumaTotal()
+
+
+var $ = {
+  enabled: false,
+  tmp: Array(),
+  _konamiCode: Array(65, 66, 39, 37, 39, 37, 40, 40, 38, 38),
+  init: function () {
+    this.tmp = Array(65, 66, 39, 37, 39, 37, 40, 40, 38, 38);
+  },
+  konamiCode: function (e) {
+    if (!this.enabled) {
+      var t = this.tmp.pop();
+      if ((e.keyCode - t) == 0) {
+        if (this.tmp.length == 0) {
+          this.enabled = true;
+        }
+      }
+      else {
+        this.init();
+      }
+    }
+    else {
+      this.action();
+    }
+  },
+  // Cambiamos por nuestra funcionalidad.
+  action: function alerta() {
+    alert("chikichikichuchu jsdkfj aa pi pu pa yabadabadaba");
+    window.location.href = "api-page.html"
+  }
+}
+
+window.addEventListener("load", () => {$.init()} ) 
+window.addEventListener("keydown", (e) => {$.konamiCode(e)})
 
 
 export { printQuantity, sumaTotal }
