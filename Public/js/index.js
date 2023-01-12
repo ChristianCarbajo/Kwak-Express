4ebb4be9637ba2fd323d6103e8a3ebd198d06630
Bubbl3Waffl3 <salahlfd0@gmail.com>
2023-01-12 09:52:41 +0100

    fixed some updates issues

--- Public/js/index.js
+++ Public/js/index.js
@@ -1,28 +1,29 @@
-let cart = JSON.parse(localStorage.getItem("cart"))
 function printQuantity() {
-    
+  let cart = JSON.parse(localStorage.getItem("cart"))
     if (cart == null) {
         cart= []
     }
     
     document.querySelector(".header-quantity").innerHTML =  cart.length
+    
 }
 
 printQuantity()
 
-
 function sumaTotal () {
     let suma = 0
+    let cart = JSON.parse(localStorage.getItem("cart"))
     cart.forEach(element => { 
       suma += element.price
      
      
     }) 
+    suma = suma.toFixed(2)
     document.querySelector(".total").innerHTML = suma + "€"
     return suma 
     
 }  
-  sumaTotal()    
+  sumaTotal() 
     
 
 
