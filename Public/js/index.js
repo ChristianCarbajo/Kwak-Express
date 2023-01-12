401bfdeffdbb25f6d590acd21f7ec64327c9f662
Bubbl3Waffl3 <salahlfd0@gmail.com>
2023-01-12 10:00:18 +0100

    quantity branch added

--- Public/js/index.js
+++ Public/js/index.js
@@ -1,30 +1,30 @@
 function printQuantity() {
   let cart = JSON.parse(localStorage.getItem("cart"))
-    if (cart == null) {
-        cart= []
-    }
-    
-    document.querySelector(".header-quantity").innerHTML =  cart.length
-    
+  if (cart == null) {
+    cart = []
+  }
+
+  document.querySelector(".header-quantity").innerHTML = cart.length
+
 }
 
 printQuantity()
 
-function sumaTotal () {
-    let suma = 0
-    let cart = JSON.parse(localStorage.getItem("cart"))
-    cart.forEach(element => { 
-      suma += element.price
-     
-     
-    }) 
-    suma = suma.toFixed(2)
-    document.querySelector(".total").innerHTML = suma + "€"
-    return suma 
-    
-}  
-  sumaTotal() 
-    
+function sumaTotal() {
+  let suma = 0
+  let cart = JSON.parse(localStorage.getItem("cart"))
+  cart.forEach(element => {
+    suma += element.price
+
+
+  })
+  suma = suma.toFixed(2)
+  document.querySelector(".total").innerHTML = suma + "€"
+  return suma
+
+}
+sumaTotal()
+
 
 
 export { printQuantity, sumaTotal }
