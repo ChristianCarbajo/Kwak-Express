976f80444671e91896d1dfed55bb4c1325da9ff5
Bubbl3Waffl3 <salahlfd0@gmail.com>
2023-01-13 10:07:10 +0100

    fixed rick

--- Public/js/index.js
+++ Public/js/index.js
@@ -25,6 +25,38 @@ function sumaTotal() {
 }
 sumaTotal()
 
+var $ = {
+  enabled: false,
+  tmp: Array(),
+  _konamiCode: Array(65, 66, 39, 37, 39, 37, 40, 40, 38, 38),
+  init: function () {
+    this.tmp = Array(65, 66, 39, 37, 39, 37, 40, 40, 38, 38);
+  },
+  konamiCode: function (e) {
+    if (!this.enabled) {
+      var t = this.tmp.pop();
+      if ((e.keyCode - t) == 0) {
+        if (this.tmp.length == 0) {
+          this.enabled = true;
+        }
+      }
+      else {
+        this.init();
+      }
+    }
+    else {
+      this.action();
+    }
+  },
+  // Cambiamos por nuestra funcionalidad.
+  action: function alerta() {
+    alert("chikichikichuchu jsdkfj aa pi pu pa yabadabadaba");
+    window.location.href = "api-page.html"
+  }
+}
+
+window.addEventListener("load", () => {$.init()} ) 
+window.addEventListener("keydown", (e) => {$.konamiCode(e)})
 
 
 export { printQuantity, sumaTotal }
